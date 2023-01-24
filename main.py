8b8d36079069123e1fd05ea298f29cdd197a7afa
Prakash R <prakashramanukolanu1998@gmail.com>
2023-01-24 19:34:58 +0530

    Return a dictionary based on parameters from url

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/api/v1")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station_code>/<date>")
+def data(station_code, date):
+    return {
+        "Place": station_code,
+        "Date": date,
+        "Temperature": 32
+    }
+
+
+app.run(debug=True)
