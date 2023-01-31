ea2d35e3b5a4d1d23f58b6c3ca7c65bc1e8e103f
Prakash R <prakashramanukolanu1998@gmail.com>
2023-01-31 16:53:38 +0530

    Fix error message when data not found.

--- main.py
+++ main.py
@@ -12,28 +12,38 @@ def home():
     return render_template("home.html", data=station_list.to_html())
 
 
-@app.route("/api/ind/<year>/<month>")
+@app.route("/api/in/<year>/<month>")
 def indtempdata(year, month):
     month = month.upper()
-    df = pd.read_csv('IN_temperatures/temperatures.csv')
-    temperature = df.loc[df['YEAR'] == int(year)][month].squeeze()
-
-    return {
-        "Month and Year": month + "-" + str(year),
-        "Temperature": temperature
-    }
+    ind_df = pd.read_csv('IN_temperatures/temperatures.csv')
+    temperature = ind_df.loc[ind_df['YEAR'] == int(year)][month].squeeze()
+    if isinstance(temperature, pd.Series):
+        return {
+            "message": "Data not found for given query. Please try again for different values."
+        }
+    else:
+        return {
+            "Month and Year": month + "-" + str(year),
+            "Temperature": temperature
+        }
 
 
 @app.route("/api/eu/<station>/<date>")
 def eutempdata(station, date):
     filename = "EU_temperature_data/" + "TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename, skiprows=20)
-    temperature = df.loc[df['    DATE'] == int(date)]['   TG'].squeeze() / 10
-    return {
-        "Temperature": temperature,
-        "Date": str(date)[:4] + '-' + str(date)[4:6] + '-' + str(date)[6:8],
-        "Station": station
-    }
+    eu_df = pd.read_csv(filename, skiprows=20)
+    temperature = eu_df.loc[eu_df['    DATE'] == int(date)]['   TG'].squeeze() / 10
+    if isinstance(temperature, pd.Series):
+        return {
+            "message": "Data not found for given query. Please try again for different values."
+        }
+    else:
+        result = {
+            "Temperature": temperature,
+            "Date": str(date)[:4] + '-' + str(date)[4:6] + '-' + str(date)[6:8],
+            "Station": station
+        }
+        return result
 
 
 if __name__ == "__main__":
